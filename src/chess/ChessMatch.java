7315faafec3fea2cf5ce1fe81d25956fee3725f5
Netto Fernandes <hugkhj@gmail.com>
2020-08-04 12:42:05 -0300

    position = matriz pos

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,8 @@
 package chess;
 
+
+
 import boardgame.Board;
-import boardgame.Position;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -23,8 +24,12 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
+	
 	private void initialSetup() {
-		board.placePiece(new Rook(board, Color.WHITE), new Position(4, 4));
-		board.placePiece(new Rook(board, Color.BLACK), new Position(5, 4));
+		placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+		
 	}
 }
